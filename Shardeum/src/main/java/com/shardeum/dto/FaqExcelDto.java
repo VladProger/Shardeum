1f77bcbb4e9309e7395d02e6b8e52319b9d88bfa
VladProger <39982246+VladProger@users.noreply.github.com>
2022-05-22 17:29:25 +0300

    Add files via upload

---
+++ Shardeum/src/main/java/com/shardeum/dto/FaqExcelDto.java
@@ -0,0 +1,14 @@
+package com.shardeum.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FaqExcelDto {
+    private Integer id;
+
+    private String question;
+
+    private String answer;
+}
---
+++ Shardeum/src/main/java/com/shardeum/dto/SearchByMessageResponse.java
@@ -0,0 +1,10 @@
+package com.shardeum.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class SearchByMessageResponse {
+    private String description;
+}
